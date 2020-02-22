cde3ec096b9f2774108da852faf9a84c760189dd
Yusuke Nojima <nojima@ynojima.com>
2020-02-22 18:11:35 +0900

    [client] Todoに作成時刻を付与

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,17 +1,21 @@
 module Main exposing (main)
 
 import Browser
+import DateFormat
 import Dict as Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes as Attr
 import Html.Events as Events
+import Task
+import Time
 
 
 main : Program () Model Message
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , update = update
+        , subscriptions = subscriptions
         , view = view
         }
 
@@ -22,6 +26,7 @@ main =
 
 type alias Model =
     { todoList : Dict TodoId Todo
+    , timeZone : Time.Zone
     }
 
 
@@ -33,27 +38,16 @@ type alias Todo =
     { id : TodoId
     , summary : String
     , done : Bool
+    , createdAt : Time.Posix
     }
 
 
-init : Model
-init =
-    { todoList =
-        Dict.fromList
-            [ ( 1
-              , { id = 1
-                , summary = "First TODO"
-                , done = True
-                }
-              )
-            , ( 2
-              , { id = 2
-                , summary = "Second TODO"
-                , done = False
-                }
-              )
-            ]
-    }
+init : () -> ( Model, Cmd Message )
+init _ =
+    ( { todoList = Dict.empty, timeZone = Time.utc }
+    , Task.map2 Initialize Time.now Time.here
+        |> Task.perform identity
+    )
 
 
 
@@ -61,21 +55,62 @@ init =
 
 
 type Message
-    = TodoChecked TodoId Bool
+    = Initialize Time.Posix Time.Zone
+    | TodoChecked TodoId Bool
 
 
-update : Message -> Model -> Model
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
     case message of
-        TodoChecked id checked ->
-            let
-                updateTodo todo =
-                    { todo | done = checked }
+        Initialize now timeZone ->
+            ( initializeTodoList now timeZone, Cmd.none )
 
-                newTodoList =
-                    Dict.update id (Maybe.map updateTodo) model.todoList
-            in
-            { model | todoList = newTodoList }
+        TodoChecked id checked ->
+            ( updateTodoCheck model id checked, Cmd.none )
+
+
+initializeTodoList : Time.Posix -> Time.Zone -> Model
+initializeTodoList now timeZone =
+    { todoList =
+        Dict.fromList
+            [ ( 1
+              , { id = 1
+                , summary = "First TODO"
+                , done = True
+                , createdAt = now
+                }
+              )
+            , ( 2
+              , { id = 2
+                , summary = "Second TODO"
+                , done = False
+                , createdAt = now
+                }
+              )
+            ]
+    , timeZone = timeZone
+    }
+
+
+updateTodoCheck : Model -> TodoId -> Bool -> Model
+updateTodoCheck model id checked =
+    let
+        updateTodo todo =
+            { todo | done = checked }
+
+        newTodoList =
+            Dict.update id (Maybe.map updateTodo) model.todoList
+    in
+    { model | todoList = newTodoList }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Message
+subscriptions _ =
+    Sub.none
 
 
 
@@ -84,12 +119,16 @@ update message model =
 
 view : Model -> Html Message
 view model =
-    ul [ Attr.class "todo-list" ]
-        (Dict.values model.todoList |> List.map renderTodo)
+    let
+        listItems =
+            Dict.values model.todoList
+                |> List.map (renderTodo model.timeZone)
+    in
+    ul [ Attr.class "todo-list" ] listItems
 
 
-renderTodo : Todo -> Html Message
-renderTodo todo =
+renderTodo : Time.Zone -> Todo -> Html Message
+renderTodo timeZone todo =
     let
         id =
             "todo-" ++ String.fromInt todo.id
@@ -97,6 +136,8 @@ renderTodo todo =
     li [ Attr.class "todo-item", Attr.id id ]
         [ renderCheckBox todo
         , renderSummary todo
+        , text " "
+        , renderCreatedAt timeZone todo
         ]
 
 
@@ -121,3 +162,30 @@ renderSummary : Todo -> Html msg
 renderSummary todo =
     span [ Attr.class "todo-item-summary" ]
         [ text todo.summary ]
+
+
+renderCreatedAt : Time.Zone -> Todo -> Html msg
+renderCreatedAt timeZone todo =
+    let
+        createdAt =
+            formatDateTime timeZone todo.createdAt
+    in
+    span [ Attr.class "todo-item-created-at" ]
+        [ text ("(" ++ createdAt ++ ")") ]
+
+
+formatDateTime : Time.Zone -> Time.Posix -> String
+formatDateTime =
+    DateFormat.format
+        [ DateFormat.yearNumber
+        , DateFormat.text "-"
+        , DateFormat.monthFixed
+        , DateFormat.text "-"
+        , DateFormat.dayOfMonthFixed
+        , DateFormat.text " "
+        , DateFormat.hourFixed
+        , DateFormat.text ":"
+        , DateFormat.minuteFixed
+        , DateFormat.text ":"
+        , DateFormat.secondFixed
+        ]
